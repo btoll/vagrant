6d60b217e1ee3d9be150e9d54d94e67345be1288
Benjamin Toll <benjam72@yahoo.com>
2021-10-01 00:37:54 -0400

    Turn on debugging for bazel vagrantfile

--- bazel/scripts/user.sh
+++ bazel/scripts/user.sh
@@ -6,7 +6,10 @@ set -euo pipefail
 wget https://github.com/bazelbuild/bazel/releases/download/4.2.1/bazel-4.2.1-installer-linux-x86_64.sh{,.sha256}
 
 # Ensure no bytes were fiddled with in-transit.
+# Turn on debugging for just this comparison.
+set -x
 [ "$(sha256sum bazel-4.2.1-installer-linux-x86_64.sh | cut -d\  -f1)" = "$(< bazel-4.2.1-installer-linux-x86_64.sh.sha256 cut -d\  -f1)" ]
+set +x
 
 chmod 755 bazel-4.2.1-installer-linux-x86_64.sh
 ./bazel-4.2.1-installer-linux-x86_64.sh --user
